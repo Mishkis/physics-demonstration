9ba5b75df417f7d9a650f352ecef9c5706cb2422
Mishkis <misha.o.gm@gmail.com>
2024-04-15 15:50:22 -0300

    Started adding collision detection

--- Scripts/acceleration.gd
+++ Scripts/acceleration.gd
@@ -2,8 +2,8 @@ extends Force
 
 @export var velocity: Force
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
 	var new_point: Vector2 = velocity.points[0] + points[0] * delta
 	
 	velocity.magnitude = new_point.length()
--- Scripts/velocity.gd
+++ Scripts/velocity.gd
@@ -2,6 +2,12 @@ extends Force
 
 @export var object: Node2D
 
+@export var normal_force: Force
 
-func _process(delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
 	object.position += points[0] * delta
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print(area.get_child(0)
